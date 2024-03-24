04fb477821fd4b28d25310de89a7d97751d6ea2f
keekuun <zkkysqs@qq.com>
2024-03-24 19:10:24 +0800

    feat: app view

---
+++ lib/app_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pizza App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Image.asset('assets/1.png'),
+    );
+  }
+}
