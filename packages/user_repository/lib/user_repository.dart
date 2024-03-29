e28bccd4e1104d4255a70bc69676f028ed2924a9
keekuun <zkkysqs@qq.com>
2024-03-29 20:09:53 +0800

    feat: update user_repo package

--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -3,4 +3,4 @@ library user_repository;
 export 'src/entities/entities.dart';
 export 'src/models/models.dart';
 export 'src/user_repository_abstract.dart';
-export 'src/firebase_user.dart';
+export 'src/firebase_user_repository.dart';
